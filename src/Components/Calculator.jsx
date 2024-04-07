31d3b207af9841b3683f98fd1a850d87dcad545d
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-04-07 13:16:29 +0530

    completed logical part of calculator_webapp

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 const Calculator = () => {
+  const [currentvalue, setcurrentvalue] = useState("");
+
+  const clickhandle = (e /*value passsed here as a parameter*/) => {
+    setcurrentvalue(currentvalue.concat(e.target.value));
+  };
+
+  const cleardata = () => {
+    setcurrentvalue("");
+  };
+
+  const calculate = () => {
+    setcurrentvalue(
+      eval(currentvalue),
+      toString() /*any error as null or not-valid should be convert into string*/
+    );
+  };
   return (
     <>
       <div
@@ -12,7 +30,8 @@ const Calculator = () => {
           <div id="input_part" className="">
             <input
               type="text"
-              className="outline-none border-[9px] border-black rounded-t-xl w-[16rem] h-[5rem] text-4xl text-right text-red-500 font-semibold font-serif "
+              className="outline-none border-[9px] border-black rounded-t-xl w-[16rem] h-[5rem] text-4xl text-right text-red-500 font-semibold font-mono "
+              value={currentvalue} //to show current value on calculator screen//
             />
           </div>
           <div
@@ -28,46 +47,122 @@ const Calculator = () => {
             <input
               type="button"
               className="outline-none bg-[#fda5a5] w-12 h-12 rounded-xl text-center text-2xl font-bold "
-              value="C"
+              value="C" //initial value of input button//
+              onClick={cleardata}
             />
-            <input type="button" className="btn" value="<" />
-            <input type="button" className="btn" value="%" />
+            <input
+              type="button"
+              className="btn"
+              value="<" //initial value of input button//
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="%" //initial value of input button//
+              onClick={clickhandle}
+            />
             <input
               type="button"
               className="outline-none bg-[#f9a9d4] w-12 h-12 rounded-xl text-center text-2xl font-bold "
-              value="/"
+              value="/" //initial value of input button//
+              onClick={clickhandle}
             />
-            <input type="button" className="btn" value="7" />
-            <input type="button" className="btn" value="8" />
-            <input type="button" className="btn" value="9" />
+            <input
+              type="button"
+              className="btn"
+              value="7" //initial value of input button//
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="8" //initial value of input button//
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="9" //initial value of input button//
+              onClick={clickhandle}
+            />
             <input
               type="button"
               className="outline-none bg-[#f9a9d4] w-12 h-12 rounded-xl text-center text-2xl font-bold "
-              value="*"
+              value="*" //initial value of input button//
+              onClick={clickhandle}
             />
-            <input type="button" className="btn" value="4" />
-            <input type="button" className="btn" value="5" />
-            <input type="button" className="btn" value="6" />
+            <input
+              type="button"
+              className="btn"
+              value="4" //initial value of input button//
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="5" //initial value of input button//
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="6"
+              onClick={clickhandle}
+            />
             <input
               type="button"
               className="outline-none bg-[#a5b4fb] w-12 h-12 rounded-xl text-center text-2xl font-bold "
               value="-"
+              onClick={clickhandle}
             />
-            <input type="button" className="btn" value="1" />
-            <input type="button" className="btn" value="2" />
-            <input type="button" className="btn" value="3" />
+            <input
+              type="button"
+              className="btn"
+              value="1"
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="2"
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="3"
+              onClick={clickhandle}
+            />
             <input
               type="button"
               className="outline-none bg-[#d8b4fe] w-12 h-12 rounded-xl text-center text-2xl font-bold "
               value="+"
+              onClick={clickhandle}
             />
-            <input type="button" className="btn" value="0" />
-            <input type="button" className="btn" value=">" />
-            <input type="button" className="btn" value="." />
+            <input
+              type="button"
+              className="btn"
+              value="0"
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value=">"
+              onClick={clickhandle}
+            />
+            <input
+              type="button"
+              className="btn"
+              value="."
+              onClick={clickhandle}
+            />
             <input
               type="button"
               className="outline-none bg-[#fdba74] w-12 h-12 rounded-xl text-center text-2xl font-bold "
               value="="
+              onClick={calculate}
             />
           </div>
         </div>
